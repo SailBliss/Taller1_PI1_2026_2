22cb858acf9efea1a6b0202bc2017903db1463cb
SailBliss <isabelitaruiz002@gmail.com>
2026-08-18 10:41:20 -0500

    creacion de app news

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from movie import views as movieViews
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", movieViews.home),
-    path("about/", movieViews.about),
+    path("", movieViews.home, name="home"),
+    path("about/", movieViews.about, name="about"),
+    path("news/", include("news.urls")),
 ]
 
 urlpatterns += static(
